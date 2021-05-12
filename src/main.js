57f1d571698c222927b0ce50e7510fcab3db48cd
Chris Zheng <z@caudate.me>
2021-05-12 13:39:13 +0800

    added context and global examples

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
+import nodeUtil from 'util'
 import React from 'react'
 import * as jotai from 'jotai'
 import * as jotaiUtils from 'jotai/utils'
-import nodeUtil from 'util'
 
 import b from './js/blessed'
 
@@ -9,32 +9,46 @@ import b from './js/blessed'
 const FlagPos = jotai.atom(true)
 
 // play.tui-store-basic.main/FlagNeg
-const FlagNeg = jotai.atom((get) => !get(FlagPos))
+const FlagNeg = jotai.atom(function (get){
+  return !get(FlagPos);
+})
 
-// play.tui-store-basic.main/App
-function App(props){
+// play.tui-store-basic.main/Button
+function Button(props){
   let [pressed,setPressed] = React.useState(false);
   let [pos,setPos] = jotai.useAtom(FlagPos);
   let [npos] = jotai.useAtom(FlagNeg);
   return (
-    <box width={30}
-      left={10}
-      top={10}><button mouse={true}
-        style={{"bg":"black","hover":{"bg":"red"}}}
-        width={30}
-        height={5}
-        onMouse={function (e){
-          if(e.button == "left"){
-            if((e.action == "mouseup")){
-              setPressed(false);
-            }
-            else{
-              setPressed(true);
-            }
+    <button mouse={true}
+      style={{"bg":"black","hover":{"bg":"red"}}}
+      width={30}
+      height={5}
+      onMouse={function (e){
+        if(e.button == "left"){
+          if((e.action == "mouseup")){
+            setPressed(false);
           }
-        }}
-        onClick={() => setPos(!pos)}
-        content={"\n" + "  " + (pressed ? "ON" : "OFF") + "\n" + "\n  " + nodeUtil.inspect({npos,pos},{"colors":true})}></button></box>
+          else{
+            setPressed(true);
+          }
+        }
+      }}
+      onClick={() => setPos(!pos)}
+      content={"\n" + "  " + props.id + " - " + (pressed ? "ON" : "OFF") + "\n" + "\n  " + nodeUtil.inspect({npos,pos},{"colors":true})}></button>
+    );
+}
+
+// play.tui-store-basic.main/App
+function App(props){
+  return (
+    <box><box left={4}
+        top={6}><jotai.Provider><Button id="context 1"></Button></jotai.Provider></box>
+      <box left={4}
+        top={12}><jotai.Provider><Button id="context 2"></Button></jotai.Provider></box>
+      <box left={4}
+        top={18}><Button id="global 1"></Button></box>
+      <box left={4}
+        top={24}><Button id="global 2"></Button></box></box>
     );
 }
 
